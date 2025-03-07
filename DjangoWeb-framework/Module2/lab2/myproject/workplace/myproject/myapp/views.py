6c262290989367dd93a2ef956fdcc9ba840a9d1b
Dushyant1607 <dushyantdhote708@gmail.com>
2025-03-07 13:22:43 +0530

    Commits

---
+++ DjangoWeb-framework/Module2/lab2/myproject/workplace/myproject/myapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse  
+from django.shortcuts import render 
+def drinks(request, drink_name):
+    drink = {
+        'mocha': 'type of coffee',
+        'tea': 'type of beverage',
+        'lemonade': 'type of refreshment'
+    }
+    choice_of_drink = drink.get(drink_name, "Sorry, this drink is not available.")
+    return HttpResponse(f"<h2>{drink_name}</h2>{choice_of_drink}")
